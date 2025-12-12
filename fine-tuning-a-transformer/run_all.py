acccb95d2a172a8ffd1b53522c394f3b8d4c29f1
Zack Hubert <zhubert@gmail.com>
2025-12-12 14:27:56 -0800

    remove old run_all.py

--- fine-tuning-a-transformer/run_all.py
+++
@@ -1,6 +0,0 @@
-import os
-import subprocess
-import glob
-
-for notebook in sorted(glob.glob('*.ipynb')):
-    subprocess.run(['jupyter', 'nbconvert', '--to', 'notebook', '--execute', notebook, '--inplace'])
